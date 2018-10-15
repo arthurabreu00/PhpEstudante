a4bcb2b04e19bdfa37bdad8535715cbc203524fc
Arthur Flávio de Abreu Tavares <277093@UTRAMIGBH.SEDE>
2018-10-15 20:19:57 -0300

    Inicial

---
+++ Aula01/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Minha primeira página scripts</title>
+</head>
+<body>
+
+    <?php
+
+        $A = 2;
+        $B = 3;
+        $soma = $A + $B;
+
+        echo "O resultado da soma é : ".$soma;
+    
+    ?>
+
+
+</body>
+</html>
