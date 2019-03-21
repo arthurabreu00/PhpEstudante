09506f7455a7958399d36e31c50b465d99e0199c
Arthur Flávio de Abreu Tavares <277093@UTRAMIGBH.SEDE>
2019-03-20 22:08:44 -0300

    Finalizando login - first project

---
+++ Aula03/includes/header.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title> Notícias </title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+
+  </head>
+  <body class="login">
+      <nav class="login-nav navbar navbar-expand-sm container mt-2 navbar-dark">
+          
+          <a class="navbar-brand" href="#"> Notícias </a>
+          <button class="navbar-toggler d-lg-none mr-sm-2" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+              aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse float-left" id="collapsibleNavId">
+              <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                  <li class="nav-item active">
+                      <a class="nav-link" href="#"> Página Inicial <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Noticias</a>
+                      <div class="dropdown-menu" aria-labelledby="dropdownId">
+                          <a class="dropdown-item" href="#">Action 1</a>
+                          <a class="dropdown-item" href="#">Action 2</a>
+                      </div>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="#"> Administração </a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="#"> Contato </a>
+                  </li>
+              </ul>
+          </div>
+      </nav>
---
+++ Aula03/login.php
@@ -0,0 +1,40 @@
+      <?php 
+        include_once('./includes/header.php')
+      ?>
+      
+      <link rel="stylesheet" href="css/login.css">
+
+      <div class="login-container container">
+          <div class="row">
+              <div class="col-sm-5">
+                  <h1 class="txt-login text-justify text-white"> Bem-Vindo </h1>
+                  <p class="sub-login text-white text-justify">
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi nostrum totam minima ex nisi. Doloremque explicabo error repellat vel magnam dignissimos ea beatae impedit, mollitia, assumenda et! Obcaecati, illum ad.
+                  </p>
+              </div>
+
+              <div class="login-col-sm-7 col-sm-7">
+                  <div class="login-form-txt">
+                      <h2>
+                          Login
+                      </h2>
+                  </div>
+                  <form class="login-form">
+        
+                        <label class="d-none" for="user"> Usuário </label>
+                        <input class="login-form-input" name="user" placeholder="Usuário" />
+            
+                      <label class="d-none" for="password"> Senha </label>
+                      <input class="login-form-input" type="password" name="password" placeholder="Senha" />
+                      <div class="login-form-txt">
+                          <a href="#"> Esqueceu a senha ? </a> 
+                      </div>
+                      <button type="submit" class="login-form-submit"> Login </button>
+                  </form>
+              </div>
+
+        </div>
+
+      <?php  
+        include_once('./includes/footer.php')
+      ?>
